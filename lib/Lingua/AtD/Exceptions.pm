4f15d0c4ca902e74f127f1df632d0311f6c0ea01
David L. Day <dday376@gmail.com>
2011-10-22 00:38:27 -0400

    Cleaned up Exceptions.

--- lib/Lingua/AtD/Exceptions.pm
+++ lib/Lingua/AtD/Exceptions.pm
@@ -4,16 +4,16 @@ package Lingua::AtD::Exceptions;
 use Exception::Class (
     Lingua::AtD::URLException => {
         fields      => [ 'url', 'host', 'port' ],
-        description => 'Indicates a malformed URL.'
+        description => 'Indicates a malformed URL.',
     },
     Lingua::AtD::HTTPException => {
         fields => [ 'http_status', 'service_url' ],
-        description => 'Indicates a problem connecting to the AtD service.'
+        description => 'Indicates a problem connecting to the AtD service.',
     },
     Lingua::AtD::ServiceException => {
         fields      => ['service_message'],
-        description => 'Indicates the AtD service returned an error message.'
-    }
+        description => 'Indicates the AtD service returned an error message.',
+    },
 );
 
 1;    # Magic true value required at end of module
